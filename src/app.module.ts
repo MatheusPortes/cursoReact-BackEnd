23330f58c51da10655b68f8740afc0704296bcea
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-07-17 16:04:40 -0300

    Conexão com banco de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataBaseModule } from './modules/data-base/data-base.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     DataBaseModule,
-    ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [AppController],
   providers: [AppService],
