b4c64788960923b5e101ffef46f0b6fc698fb246
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-07-21 11:42:43 -0300

    M04-A08-V0.0.1: Criação da migrations address.

---
+++ src/migration/1658372894187-Address.migration.ts
@@ -0,0 +1,67 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class Addressmigration1658372894187 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable( new Table({
+            name: 'address',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'serial',
+                    isPrimary: true
+                },
+                {
+                    name: 'id_city',
+                    type: 'integer',
+                },
+                {
+                    name: 'cep',
+                    type: 'integer',
+                },
+                {
+                    name: 'neighborhood',
+                    type: 'varchar',
+                    length: '255',
+                },
+                {
+                    name: 'number',
+                    type: 'integer',
+                },
+                {
+                    name: 'street',
+                    type: 'varchar',
+                    length: '255',
+                },
+                {
+                    name: 'complement',
+                    type: 'varchar',
+                    length: '255',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()'
+                },
+                {
+                    name: 'update_at',
+                    type: 'timestamp',
+                },
+            ],
+            foreignKeys: [
+                {
+                    columnNames: ['id_city'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'city',
+                    onDelete: "CASCADE",
+                    onUpdate: "CASCADE"
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('address')
+    }
+
+}
