8f18d451c036e2c01185aef71986eb1f5a1117b6
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-07-23 16:23:45 -0300

    M04-A08-V0.0.1: Modificação da entitys Person.

--- src/entities/Person.entity.ts
+++ src/entities/Person.entity.ts
@@ -1,6 +1,29 @@
-import { Entity } from "typeorm"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import { User } from "./User.entity"
 
 @Entity()
-export class Person.entity {
+export class Person {
+  @PrimaryGeneratedColumn()
+  id: number
 
+  @Column({ type: 'varchar', length: 255 })
+  name: number
+
+  @Column({ type: 'varchar', length: 11 })
+  cpf: string
+
+  @Column({  type: 'varchar', length: 10 })
+  rg: string
+
+  @Column({ type: 'date' })
+  birth_date: Date
+
+  @Column({ type: 'timestamp' })
+  update_at: Date
+
+  @Column({ type: 'timestamp' })
+  created_at: Date
+
+  @OneToMany(() => User, user => user.person)
+  user: User
 }
