cdf5ebe51d91bd264f594cafbca272f6dbb8fa65
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-07-23 16:24:14 -0300

    M04-A08-V0.0.1: Modificação da entitys Country.

--- src/entities/Country.entity.ts
+++ src/entities/Country.entity.ts
@@ -1,6 +1,26 @@
-import { Entity } from "typeorm"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import { State } from "./State.entity"
 
 @Entity()
-export class Country.entity {
+export class Country {
+  @PrimaryGeneratedColumn()
+  id: number
 
+  @Column({ type: 'varchar', length: 60 })
+  name: string
+
+  @Column({ type: 'varchar', length: 60 })
+  name_tpbr: string
+
+  @Column({ type: 'varchar', length: 2, nullable: true })
+  initials: string
+
+  @Column({ type: 'integer' })
+  bacen: string
+
+  @Column({ type: 'timestamp' })
+  created_at: Date
+
+  @OneToMany(() => State, state => state.country)
+  state: State[]
 }
