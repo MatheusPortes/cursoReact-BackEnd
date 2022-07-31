111a0ce1493829137f7b1925630f3cad86c137f2
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-07-31 17:53:22 -0300

    M04-A08-V0.0.1: Criação dos Filtros de exceção.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataBaseModule } from './modules/data-base/data-base.module';
+import { AddressModule } from './modules/address/address.module';
+import { UnitOfWorkModule } from './modules/unit-of-work/unit-of-work.module';
 
 @Module({
   imports: [
     DataBaseModule,
+    AddressModule,
+    UnitOfWorkModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/common/http-exception.filter.ts
+++ src/common/http-exception.filter.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
