d58f469d01e9c6719a356b659367d42303914815
Matheus Portes Lacerda <portes.matheus@outlook.com>
2022-08-03 12:44:23 -0300

    M04-A08-V0.0.2: Melhoria do sistema de Filtros de exceção.

--- src/common/http-exception.filter.ts
+++ src/common/http-exception.filter.ts
@@ -1,21 +1,54 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common'
+import { Request, Response } from 'express'
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<Request>()
     
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    const status = exception.getStatus()
+    const constructor = exception.name
+    const message = exception.message
+    const error: any = exception.getResponse()
 
-    response
-      .status(status)
-      .json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
+    let custom_message = {}
+    custom_message = { constructor_name: constructor }
+
+    if (constructor === 'NotFoundException') {
+      custom_message = {
+        ...custom_message,
+        message: error.error,
+        error: message,
+      }
+    }
+
+    if (constructor === 'BadRequestException') {
+      custom_message = {
+        ...custom_message,
+        message: message,
+        error: error.error,
+      }
+    }
+
+    if (constructor === 'ForbiddenException') {
+      custom_message = {
+        ...custom_message,
+        message: message,
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      ...custom_message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    })
   }
 }
